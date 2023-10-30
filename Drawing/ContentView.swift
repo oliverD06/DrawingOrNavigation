3f664bee314ef460e95522a3945ce5477e76a9be
Oliver Delaney <oliver.delaney@stu.barren.kyschools.us>
2023-10-30 09:48:38 -0500

    Initial Commit

---
+++ Drawing/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Drawing
+//
+//  Created by Oliver Delaney on 10/30/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Drawing/DrawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingApp.swift
+//  Drawing
+//
+//  Created by Oliver Delaney on 10/30/23.
+//
+
+import SwiftUI
+
+@main
+struct DrawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
